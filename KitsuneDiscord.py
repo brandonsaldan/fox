2817048c815bc0ec8c8beb01da30ad4cf173b752
bren <26472557+brensalsa@users.noreply.github.com>
2018-05-15 02:08:13 -0400

    cleaned up grammar, punctuation, and fixed consistency

--- KitsuneDiscord.py
+++ KitsuneDiscord.py
@@ -1,5 +1,5 @@
 # ported to discord by exofeel
-# orginally coded by @4201337
+# orginally coded by 4201337
 # ported to python3 by brensalsa
 
 import discord
@@ -38,18 +38,18 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def kitsune_help(ctx):
-    embed = discord.Embed(title="Kitsune is a simple discord bot that can do social network scraping!", colour=discord.Colour(0xffb25d), description="Please read fully all commands.", timestamp=datetime.datetime.utcfromtimestamp(1526316493))
+    embed = discord.Embed(title="Kitsune is a simple social media username scraping Discord bot.", colour=discord.Colour(0xffb25d), description="Please fully read all commands.", timestamp=datetime.datetime.utcfromtimestamp(1526316493))
 
 #embed.set_image()
 #embed.set_thumbnail()
     embed.set_author(name="Kitsune Help Command")
     embed.set_footer(text="Kitsune")
 
-    embed.add_field(name="Default Prefix", value="the prefix currently is %. We will add support for changing the prefix in chat. However you can change the prefix in the source code.")
-    embed.add_field(name="%kitsune", value="Usage: \n```%kitsune [network] [username]``` \nThe current supported social networks are \n\n-Reddit\n-Instagram\n-Twitter\n- and YouTube.\n")
-    embed.add_field(name="%batch", value="Kitsune can do batches of usernames inside a .txt file. Please. Seperate each username with a different line. Any other can break the bot and make the social network think weirdly about future requests.\n\nUsage:\n```%batch [network] [url to .txt file]```")
+    embed.add_field(name="Default prefix", value="the default prefix is currently %. We will add support for changing the prefix in chat, however you can change the prefix in the source code.")
+    embed.add_field(name="%kitsune", value="Usage: \n```%kitsune [network] [username]``` \nThe current supported social networks are: \n\n-Reddit\n-Instagram\n-Twitter\n-YouTube.\n")
+    embed.add_field(name="%batch", value="Kitsune can do batches of usernames inside a .txt file. Please seperate each username into it's own line. Any other format can break the bot and make the social network think weirdly about future requests.\n\nUsage:\n```%batch [network] [url to .txt file]```")
     embed.add_field(name="Have a suggestion?", value="**u/exofeel**\n\n**Garrett#8026**\n\n**/u/lafterr**\n\n**Padraig#1020**", inline=True)
-    embed.add_field(name="Donate?", value="Sure we take cypto.\n \n**VTC**:*VhYdqgeBmudv3wuDwNB4VhuQzNjK3Rw9n7* \n**ETH**:*0x6CC93E2E2D4dd0430Bab5d8Bb71a395090B84026*", inline=True)
+    embed.add_field(name="Donations?", value="\n \n**Vertcoin**:*VhYdqgeBmudv3wuDwNB4VhuQzNjK3Rw9n7* \n**Ethereum**:*0x6CC93E2E2D4dd0430Bab5d8Bb71a395090B84026*", inline=True)
 
     await bot.say(embed=embed)
 
@@ -67,7 +67,7 @@ async def batch(ctx, network, url, force="false"):
         username_count = len(usernames)
         if force == "true":
             if username_count >= 999:
-                await bot.say("I'm sorry but 75 usernames is the limit.\nPlease, remove some before continuing.")
+                await bot.say("I'm sorry, but 75 usernames is the limit.\nPlease remove some before continuing.")
             else:
                 pass   
         else:
@@ -82,18 +82,18 @@ async def batch(ctx, network, url, force="false"):
                     embed = discord.Embed(title="Kitsune Username Scraper.", colour=discord.Colour(0xfe139e), description="Loading!", timestamp=datetime.datetime.utcfromtimestamp(1526305073))
 
                     embed.set_thumbnail(url="http://www.slashfilm.com/wp/wp-content/images/Zootopia-Nick-Wilde.jpg")
-                    embed.set_footer(text="Kitsune username scraper")
+                    embed.set_footer(text="Kitsune Username Scraper")
 
                     embed.add_field(name="Available names ✅", value="{}".format(available_names))
                     embed.add_field(name="Unavailable names :no_entry:", value="{}".format(unavailable_names))
-                    embed.add_field(name="Names that returned an error. :warning:", value="{}".format(error_names))
+                    embed.add_field(name="Names that returned an error :warning:", value="{}".format(error_names))
 
                     message = await bot.say(embed=embed)
                     message
                     for x in usernames:
                         print("Checking username {}".format(x))
                         social_url = social_networks[network]
-                        req = requests.get(social_url + x, headers = {'User-agent': 'Kitsune username checker'})
+                        req = requests.get(social_url + x, headers = {'User-agent': 'Kitsune Username Scraper'})
                         username_prefix = social_network_prefix[network]
                         if req.status_code == 200:
                             unavailable_names.append(x)
@@ -121,11 +121,11 @@ async def batch(ctx, network, url, force="false"):
                             embed.set_thumbnail(url="http://logos-download.com/wp-content/uploads/2016/02/Twitter_logo_bird_transparent_png-700x568.png")
                             pass
                         elif network == "reddit":
-                            embed = discord.Embed(title="reddit Username Scraper", colour=discord.Colour(0xf08a0d), description="A total number of {} were scraped.".format(len(usernames)), timestamp=datetime.datetime.utcfromtimestamp(1526305073))
+                            embed = discord.Embed(title="Reddit Username Scraper", colour=discord.Colour(0xf08a0d), description="A total number of {} were scraped.".format(len(usernames)), timestamp=datetime.datetime.utcfromtimestamp(1526305073))
                             embed.set_thumbnail(url="https://is5-ssl.mzstatic.com/image/thumb/Purple125/v4/36/b8/a9/36b8a9e3-55ff-d52c-ea7e-974414f925f0/source/100x100bb.jpg")                         
                         else:
-                            await bot.say("error? did you use an actual network, if so how did you get past?")
-                        embed.set_footer(text="Kitsune username scraper")
+                            await bot.say("Error? did you use an actual network, if so how did you get past?")
+                        embed.set_footer(text="Kitsune Username Scraper")
 
                         embed.add_field(name="Available names ✅", value="{}".format(available_names))
                         embed.add_field(name="Unavailable names :no_entry:", value="{}".format(unavailable_names))
@@ -142,7 +142,7 @@ async def kitsune(ctx, network, username):
     try:
         try:
             social_url = social_networks[network]
-            req = requests.get(social_url + username, headers = {'User-agent': 'Kitsune username checker'})
+            req = requests.get(social_url + username, headers = {'User-agent': 'Kitsune Username Scraper'})
             username_prefix = social_network_prefix[network]
             if req.status_code == 200:
                 #await bot.say("{}{} is unavailable.".format(username_prefix, username))
